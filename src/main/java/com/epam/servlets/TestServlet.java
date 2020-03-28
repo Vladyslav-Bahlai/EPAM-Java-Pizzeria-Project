3422d1a80d9af2ccbdc143d9a73780991c69acfc
Vladyslav-Bahlai <54863517+Vladyslav-Bahlai@users.noreply.github.com>
2020-03-28 17:47:20 +0200

    added web.xml file, created and cofigured TestServlet class, set up Tomcat server

---
+++ src/main/java/com/epam/servlets/TestServlet.java
@@ -0,0 +1,24 @@
+package com.epam.servlets;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet("/TestServlet")
+public class TestServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.setContentType("text/html");
+        PrintWriter out = response.getWriter();
+        out.println("<h3>Hello, Vlad</h3>");
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doPost(req, resp);
+    }
+}
